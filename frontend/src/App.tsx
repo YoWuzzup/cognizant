f40e4e26b00e4accb75dbf4fd9f6bab9333cb9cf
YoWuzzup <YoWuzzup@live.com>
2022-02-27 22:43:16 +0100

    added pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+
+import { NavBar } from "./components";
+import { Home, Cart, Item } from "./pages";
+
 export const App: React.FC = () => {
-  return <>asd</>;
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/cars/:id" element={<Item />} />
+      </Routes>
+    </>
+  );
 };
